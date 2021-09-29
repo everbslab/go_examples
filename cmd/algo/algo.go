2b13c591673cbb227775f42532ec59a5d11e5c66
Evgeny Verbitsky <everbslab@gmail.com>
2021-09-29 13:22:40 +0700

    refactored binary search: case for unsorted slice

--- cmd/algo/algo.go
+++ cmd/algo/algo.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	rndRangeSz := 1_000_000
+	rndRangeSz := 1000
 	binarySearchExample(rndRangeSz)
 }
 
@@ -20,15 +20,15 @@ func binarySearchExample(sz int) {
 
 	haystack := make([]int, sz)
 	for i := 0; i <= len(haystack)-1; i++ {
-		haystack[i] = i
+		haystack[i] = rand.Int()
 	}
 
-	logRes := math.Round(math.Log2(float64(sz)))
+	var logRes = math.Round(math.Log2(float64(sz)))
 	fmt.Printf("Algo complexity: O^(log n). Log2(%d) = %.0f\n", sz, logRes)
 
-	needle := haystack[rand.Intn(len(haystack))] // Pick random needle value
+	needle := haystack[rand.Intn(len(haystack)-1)] // Pick random needle value
 
-	if ok, index := algo.BinarySearch(haystack, needle); ok {
+	if index, ok := algo.BinarySearch(haystack, needle); ok {
 		fmt.Printf("Value `%d` was found by index `%d`.\n", needle, index)
 	} else {
 		fmt.Printf("Value `%d` was NOT found.\n", needle)
